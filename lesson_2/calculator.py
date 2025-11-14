42385b833aaef3919b3f89abe57221a3141d6bdb
Steve Milanes <209536378+WanderingMountainLight@users.noreply.github.com>
2025-11-13 21:49:27 -0500

    Refactor: created helpers, divide by zero check, and wrap in main()

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -13,70 +13,90 @@ def invalid_number(number_str):
         return True
     return False
 
-prompt("Select language: 1) English 2) Espanol 3) Português")
-lang_choice = input()
+def prompt_for_num(prompt_message, error_message):
+    prompt(prompt_message)
+    num = input()
 
-while lang_choice not in ['1', '2', '3']:
-    prompt('Invalid choice. Please select 1, 2, or 3')
+    while invalid_number(num):
+        prompt(error_message)
+        num = input()
+
+    return num
+
+
+def operation_choice(num1, num2, msg):
+    prompt(msg['choose_operation'])
+    operation_num = input()
+
+    while operation_num not in ['1', '2', '3','4']:
+        prompt(msg['invalid_operation'])
+        operation_num = input()
+
+    if operation_num == '4':
+        while invalid_number(num2) or float(num2) == 0:
+            if invalid_number(num2):
+                prompt(msg['invalid_num'])
+                num2 = input()
+            else:
+                prompt(msg['divide_by_zero'])
+                num2 = input()
+
+    match operation_num:
+        case '1': # '1' represents Addition
+            calculation_result = float(num1) + float(num2)
+        case '2': # '2' represents Subtraction
+            calculation_result = float(num1) - float(num2)
+        case '3': # '3' represents Multiplication
+            calculation_result = float(num1) * float(num2)
+        case '4': # '4' represents Division
+            calculation_result = float(num1) / float(num2)
+
+    return calculation_result
+
+def main():
+    prompt("Select language: 1) English 2) Espanol 3) Português")
     lang_choice = input()
 
-if lang_choice == '1':
-    LANG = 'english'
-elif lang_choice == '2':
-    LANG = 'spanish'
-elif lang_choice == '3':
-    LANG = 'portuguese'
+    while lang_choice not in ['1', '2', '3']:
+        prompt('Invalid choice. Please select 1, 2, or 3')
+        lang_choice = input()
 
-messages = all_messages[lang_choice]
+    if lang_choice == '1':
+        lang = 'english'
+    elif lang_choice == '2':
+        lang = 'spanish'
+    else:
+        lang = 'portuguese'
 
+    messages = all_messages[lang]
 
-prompt(messages['welcome'])
+    prompt(messages['welcome'])
 
-continue_cal = True
+    continue_calc = True
 
-while continue_cal:
-    prompt(messages['first_num'])
-    number1 = input()
+    while continue_calc:
+        number1 = prompt_for_num(messages['first_num'],
+                                 messages['invalid_num'])
 
-    while invalid_number(number1):
-        prompt(messages['invalid_num'])
-        number1 = input()
-    prompt(messages['second_num'])
-    number2 = input()
+        number2 = prompt_for_num(messages['second_num'],
+                                 messages['invalid_num'])
 
-    while invalid_number(number2):
-        prompt(messages['invalid_num'])
-        number2 = input()
+        result = operation_choice(number1, number2, messages)
 
-    prompt(messages['choose_operation'])
-    operation = input()
+        prompt(f'The result is {result}.')
 
-    while operation not in ['1', '2', '3','4']:
-        prompt(messages['invalid_operation'])
-        operation = input()
-
-    match operation:
-        case '1': # '1' represents Addition
-            result = float(number1) + float(number2)
-        case '2': # '2' represents Subtraction
-            result = float(number1) - float(number2)
-        case '3': # '3' represents Multiplication
-            result = float(number1) * float(number2)
-        case '4': # '4' represents Division
-            result = float(number1) / float(number2)
-    prompt(f'The result is {result}.')
-    prompt(messages['another_calc'])
-    answer = input()
-    answer_str = answer.title()
-
-    while answer_str not in [messages['yes_response'],
-                             messages['no_response']]:
-        prompt(messages['invalid_yes_no'])
+        prompt(messages['another_calc'])
         answer = input()
         answer_str = answer.title()
 
-    if answer_str == messages['yes_response']:
-        continue_cal = True
-    elif answer_str == messages['no_response']:
-        continue_cal = False
-        prompt(messages['thank_you'])
+        while answer_str not in [messages['yes_response'],
+                                messages['no_response']]:
+            prompt(messages['invalid_yes_no'])
+            answer = input()
+            answer_str = answer.title()
+
+        if answer_str == messages['no_response']:
+            continue_calc = False
+            prompt(messages['thank_you'])
+
+main()
