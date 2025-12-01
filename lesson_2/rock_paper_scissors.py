16dec71714afce0860328a2980b6548dfb1401c8
Steve Milanes <209536378+WanderingMountainLight@users.noreply.github.com>
2025-12-01 17:53:39 -0500

    added best of 5 counter and loop

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -1,5 +1,9 @@
 import random
 
+
+user_count = 0
+cpu_count = 0
+
 VALID_CHOICES = ['rock', 'paper', 'scissors']
 
 def prompt(message):
@@ -10,27 +14,92 @@ def display_winner(player, computer):
         (player == 'paper' and computer == 'rock') or
         (player == 'scissors' and computer == 'paper')):
         prompt('You win')
+        return 'You win'
     elif((player == 'rock' and computer == 'paper') or
         (player == 'paper' and computer == 'scissors') or
         (player == 'scissors' and computer == 'rock')):
         prompt('Computer wins')
+        return 'Computer wins'
     else:
-        prompt("It's a tie!")
+        return "It's a tie!"
 
-while True:
-    prompt(f"Choose one: {', '.join(VALID_CHOICES)}")
-    choice = input()
+def games_won(winner):
+    global user_count, cpu_count
+    if winner == 'You win':
+        user_count += 1
+        prompt(f'You have won {user_count} games.')
+        return user_count
+    elif winner == 'Computer wins':
+        cpu_count += 1
+        prompt(f'The computer has won {cpu_count} games.')
+        return cpu_count
+    else:
+        prompt("It's a tie. Let's try again!")
+    
+def score_message(result):
+    global user_count, cpu_count
+    if result == 'You win':
+        if user_count < 5:
+            prompt(f'''You won that round.
+                The current score is: 
+                User {user_count} games.
+                Computer {cpu_count} games.
+                Whomever wins 5 first, wins the match''')
+            return True
+        else:
+            prompt(f'''You won.
+                Final score:
+                User {user_count}
+                Computer {cpu_count}''')
+            return False
+            
+    elif result == 'Computer wins':
+        if cpu_count < 5:
+            prompt(f'''You lost that round.
+                The current score is: 
+                User {user_count} games.
+                Computer {cpu_count} games.
+                Whomever wins 5 first, wins the match''')
+            return True
+        else:
+            prompt(f'''You Lost.
+                Final score:
+                Computer {cpu_count}
+                User {user_count}''')
+            return False
+    else:
+        prompt(f'''The current score is: 
+                User {user_count} games.
+                Computer {cpu_count} games.
+                Whomever wins 5 first, wins the match''')
+        return True
 
-    while choice not in VALID_CHOICES:
-        prompt(f'''That is not a vaild choice.
-               Please choose one: {', '.join(VALID_CHOICES)}''')
+def main():
+    while True:
+
+        prompt(f"Choose one: {', '.join(VALID_CHOICES)}")
         choice = input()
 
-    cpu_choice = random.choice(VALID_CHOICES)
+        while choice not in VALID_CHOICES:
+            prompt(f'''That is not a vaild choice.
+                Please choose one: {', '.join(VALID_CHOICES)}''')
+            choice = input()
 
-    prompt(f'You chose {choice}. The computer chose {cpu_choice}.')
+        cpu_choice = random.choice(VALID_CHOICES)
 
-    display_winner(choice, cpu_choice)
+        prompt(f'You chose {choice}. The computer chose {cpu_choice}.')
+
+        result = display_winner(choice, cpu_choice)
+        
+        games_won(result)
+
+        keep_playing = score_message(result)
+
+        if keep_playing == False:
+            break
+
+while True:
+    main()
 
     prompt("Would you like to play again? (y/n)")
     answer = input().lower()
@@ -44,3 +113,6 @@ while True:
     if answer[0] == 'n':
         prompt('Thanks for playing!')
         break
+    elif answer[0] == 'y':
+        user_count = 0
+        cpu_count = 0
