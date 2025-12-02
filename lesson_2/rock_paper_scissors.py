e985a2ad1b106086661ef2ae31ccbcc4f1cd3098
Steve Milanes <209536378+WanderingMountainLight@users.noreply.github.com>
2025-12-02 17:32:12 -0500

    Added Lizard Spock options, refactored code for improved flexibility

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -4,40 +4,44 @@ import random
 user_count = 0
 cpu_count = 0
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = {
+    'r': 'rock',
+    'p': 'paper',
+    'sci': 'scissors',
+    'liz': 'lizard',
+    'spo': 'spock'}
+
+WINNING_COMBOS = {
+    'rock' : ['lizard','scissors'],
+    'paper' : ['rock', 'spock'],
+    'scissors' : ['paper', 'lizard'],
+    'lizard' : ['spock', 'paper'],
+    'spock' : ['scissors', 'rock']
+}
 
 def prompt(message):
     print(f'===> {message}')
 
 def display_winner(player, computer):
-    if((player == 'rock' and computer == 'scissors') or
-        (player == 'paper' and computer == 'rock') or
-        (player == 'scissors' and computer == 'paper')):
+    if computer in WINNING_COMBOS[player]:
         prompt('You win')
         return 'You win'
-    elif((player == 'rock' and computer == 'paper') or
-        (player == 'paper' and computer == 'scissors') or
-        (player == 'scissors' and computer == 'rock')):
+    if player in WINNING_COMBOS[computer]:
         prompt('Computer wins')
         return 'Computer wins'
-    else:
-        return "It's a tie!"
+    return "It's a tie!"
 
 def games_won(winner):
     global user_count, cpu_count
     if winner == 'You win':
         user_count += 1
         prompt(f'You have won {user_count} games.')
-        return user_count
-    elif winner == 'Computer wins':
+    if winner == 'Computer wins':
         cpu_count += 1
         prompt(f'The computer has won {cpu_count} games.')
-        return cpu_count
-    else:
-        prompt("It's a tie. Let's try again!")
-    
+    prompt("It's a tie. Let's try again!")
+
 def score_message(result):
-    global user_count, cpu_count
     if result == 'You win':
         if user_count < 5:
             prompt(f'''You won that round.
@@ -46,14 +50,13 @@ def score_message(result):
                 Computer {cpu_count} games.
                 Whomever wins 5 first, wins the match''')
             return True
-        else:
-            prompt(f'''You won.
-                Final score:
-                User {user_count}
-                Computer {cpu_count}''')
-            return False
-            
-    elif result == 'Computer wins':
+        prompt(f'''You won.
+            Final score:
+            User {user_count}
+            Computer {cpu_count}''')
+        return False
+
+    if result == 'Computer wins':
         if cpu_count < 5:
             prompt(f'''You lost that round.
                 The current score is: 
@@ -61,41 +64,47 @@ def score_message(result):
                 Computer {cpu_count} games.
                 Whomever wins 5 first, wins the match''')
             return True
-        else:
-            prompt(f'''You Lost.
-                Final score:
-                Computer {cpu_count}
-                User {user_count}''')
-            return False
-    else:
-        prompt(f'''The current score is: 
-                User {user_count} games.
-                Computer {cpu_count} games.
-                Whomever wins 5 first, wins the match''')
-        return True
+        prompt(f'''You Lost.
+            Final score:
+            Computer {cpu_count}
+            User {user_count}''')
+        return False
+    prompt(f'''The current score is:
+            User {user_count} games.
+            Computer {cpu_count} games.
+            Whomever wins 5 first, wins the match''')
+    return True
 
 def main():
+    prompt('Welcome to Rock, Paper, Scissors, Lizard, Spock!')
+
     while True:
 
-        prompt(f"Choose one: {', '.join(VALID_CHOICES)}")
-        choice = input()
+        choices_list = []
+        for shortcut, full_name in VALID_CHOICES.items():
+            choices_list.append(f"{shortcut} for {full_name.title()}")
+        choices_string = ", ".join(choices_list)
 
-        while choice not in VALID_CHOICES:
-            prompt(f'''That is not a vaild choice.
-                Please choose one: {', '.join(VALID_CHOICES)}''')
-            choice = input()
+        prompt(f"Choose one: {choices_string}")
+        user_choice = input()
 
-        cpu_choice = random.choice(VALID_CHOICES)
+        while user_choice not in VALID_CHOICES:
+            prompt(f'That is not a valid choice. Choose one: {choices_string}')
+            user_choice = input()
 
-        prompt(f'You chose {choice}. The computer chose {cpu_choice}.')
+        user_choice = VALID_CHOICES[user_choice]
 
-        result = display_winner(choice, cpu_choice)
-        
+        cpu_choice = random.choice(list(VALID_CHOICES.values()))
+
+        prompt(f'You chose {user_choice}. The computer chose {cpu_choice}.')
+
+        result = display_winner(user_choice, cpu_choice)
+
         games_won(result)
 
         keep_playing = score_message(result)
 
-        if keep_playing == False:
+        if not keep_playing:
             break
 
 while True:
@@ -113,6 +122,5 @@ while True:
     if answer[0] == 'n':
         prompt('Thanks for playing!')
         break
-    elif answer[0] == 'y':
-        user_count = 0
-        cpu_count = 0
+    user_count = 0
+    cpu_count = 0
