3da407d4353cbc4b01d80107467c616c7ea47bb2
Steve Milanes <209536378+WanderingMountainLight@users.noreply.github.com>
2025-12-06 15:53:06 -0500

    Started practice 4: medium problems

--- lesson_3/practice_4_med.py
+++ lesson_3/practice_4_med.py
@@ -5,3 +5,45 @@
 
 phrase = 'The Flintstones Rock!'
 
+for count in range(1,11):
+    print(('-' * count) + phrase)
+
+
+#Question 2
+#Alan wrote the following function, which was intended to return all of the factors of number:
+
+#Reprompt solution
+def factors(number):
+    divisor = number
+    result = []
+    while divisor <= 0:
+        print('Cannot divide by negative numebrs or zero. Please input a number greater than 0.')
+        number = int(input())
+        divisor = number
+    while divisor != 0:
+        if number % divisor == 0:
+            result.append(number // divisor)
+        divisor -= 1
+    return result
+
+print(factors(0))
+
+#Graceful handling of edge cases and small change.
+
+def factors(number):
+    divisor = number
+    result = []
+    while divisor > 0:
+        if number % divisor == 0:
+            result.append(number // divisor)
+        divisor -= 1
+    return result
+
+print(factors(10))
+
+#Alyssa noticed that this code would fail when the input is a negative number, and asked Alan to change the loop.
+#How can he make this work? Note that we're not looking to find the factors for negative numbers,
+#but we want to handle it gracefully instead of going into an infinite loop.
+
+#Bonus Question: What is the purpose of number % divisor == 0 in that code?
+#To ensure clean division so the only returns are factors of the number input.
