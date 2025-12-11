a766f98a5b56d1a717412f4964509d16a1133824
Steve Milanes <209536378+WanderingMountainLight@users.noreply.github.com>
2025-12-11 13:50:36 -0500

    Remaining problems from Easy 1 completed

---
+++ practice_problems/multi_3_5.py
@@ -0,0 +1,26 @@
+# Multiples of 3 or 5
+# Write a function that computes the sum of all numbers between 1 and some other number, inclusive, that are multiples of 3 or 5.
+#For instance, if the supplied number is 20, the result should be 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20).
+
+# You may assume that the number passed in is an integer greater than 1.
+
+# Examples
+# # These examples should all print True
+# print(multisum(3) == 3)
+# print(multisum(5) == 8)
+# print(multisum(10) == 33)
+# print(multisum(1000) == 234168)
+
+def multisum(num):
+    number = 1
+    total = 0
+    while number <= num:
+        if number % 3 == 0 or number % 5 == 0:
+            total += number
+        number += 1
+    return total
+
+print(multisum(3) == 3)
+print(multisum(5) == 8)
+print(multisum(10) == 33)
+print(multisum(1000) == 234168)
---
+++ practice_problems/sls.py
@@ -0,0 +1,23 @@
+# Short Long Short
+# Write a function that takes two strings as arguments, 
+# determines the length of the two strings, 
+# and then returns the result of concatenating the shorter string,
+# the longer string, and the shorter string once again.
+# You may assume that the strings are of different lengths.
+
+# Examples
+# # These examples should all print True
+# print(short_long_short('abc', 'defgh') == "abcdefghabc")
+# print(short_long_short('abcde', 'fgh') == "fghabcdefgh")
+# print(short_long_short('', 'xyz') == "xyz")
+
+def sls(string1, string2):
+    if len(string1) > len(string2):
+        return string2 + string1 + string2
+    else:
+        return string1 + string2 + string1
+
+
+print(sls('abc', 'defgh')) #abcdefghabc
+print(sls('abcde', 'fgh')) #fghabcdefgh
+print(sls('', 'xyz')) #xyz
